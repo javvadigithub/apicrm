389bf75d54afc85f2bcfc6644250662cd64dcac1
javvadigithub <j_srikanth@outlook.com>
2020-04-09 22:47:59 +0530

    Static Files Updated

--- crm/inv/models.py
+++ crm/inv/models.py
@@ -7,16 +7,16 @@ class Customer(models.Model):
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    
+
     def __str__(self):
         return self.name
 
 class Tag(models.Model):
     name = models.CharField(max_length=200, null=True)
-    
+
     def __str__(self):
         return self.name
-    
+
 class Product(models.Model):
     CATEGORY = (
             ('Indoor', 'Indoor'),
@@ -28,10 +28,10 @@ class Product(models.Model):
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     tags = models.ManyToManyField(Tag)
-    
+
     def __str__(self):
         return self.name
-    
+
 class Order(models.Model):
     STATUS = (
             ('Pending', 'Pending'),
@@ -43,7 +43,6 @@ class Order(models.Model):
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
     # note = models.CharField(max_length=1000, null=True)
-    
+
     def __str__(self):
         return self.product.name
-    
--- crm/inv/views.py
+++ crm/inv/views.py
@@ -1,41 +1,53 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
 from .models import *
 from .forms import OrderForm
 # from .filters import OrderFilter
 # Create your views here.
 
+def registerPage(request):
+    form = UserCreationForm
+    context = {}
+    return render(request, 'register.html', context)
+
+
+def loginPage(request):
+    context = {}
+    return render(request, 'login.html', context)
+
+
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
-    
+
     total_customers = customers.count()
     total_orders = Order.objects.count()
     delivered = orders.filter(status='Delivered').count()
     pending = orders.filter(status='Pending').count()
-    
+
     context = {'orders': orders, 'customers': customers, 'total_customers': total_customers,
     'total_orders':total_orders, 'delivered':delivered, 'pending':pending}
     return render(request, 'dashboard.html', context)
 
 def products(request):
     products = Product.objects.all()
-    
+
     return render(request, 'products.html', {'products': products})
 
 def customer(request, pk_test):
     customer = Customer.objects.get(id=pk_test)
-    
+
     orders = customer.order_set.all()
     orders_count = orders.count()
-               
+
     # myFilter = OrderFilter(request.GET, queryset=orders)
     # orders = myFilter.qs
-    
+
     context = {'customer':customer, 'orders':orders, 'orders_count':orders_count}
     return render(request, 'customer.html', context)
-    
+
 def createOrder(request, pk):
     OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=5)
     customer = Customer.objects.get(id=pk)
@@ -47,22 +59,22 @@ def createOrder(request, pk):
         if formset.is_valid():
             formset.save()
             return redirect('/')
-        
+
     context = {'formset':formset}
     return render(request, 'orderform.html', context)
 
 
 def updateOrder(request, pk):
-    
+
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
-    
+
     if request.method == 'POST':
         form = OrderForm(request.POST, instance=order)
         if form.is_valid():
             form.save()
             return redirect('/')
-        
+
     context = {'form':form}
     return render(request, 'orderform.html', context)
 
@@ -71,6 +83,6 @@ def deleteOrder(request, pk):
     if request. method == "POST":
         order.delete()
         return redirect('/')
-    
+
     context = {}
     return render(request, 'delete.html', context)
